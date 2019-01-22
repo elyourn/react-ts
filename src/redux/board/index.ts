51448da85d7d8fa9cc0bbbd99181e10cb96bb850
elyourn <elyourn52@gmail.com>
2019-01-22 20:52:06 +0300

    Auto stash before merge of "master" and "ts/master"

--- src/redux/board/index.ts
+++ src/redux/board/index.ts
@@ -12,17 +12,17 @@ export type BOARD_LOAD = typeof BOARD_LOAD;
 export type BOARD_LOADED_SUCCESS = typeof BOARD_LOADED_SUCCESS;
 
 export type BoardItem = {
-  id: Id;
-  tags: string[];
-  name: string;
-  position: number;
+	id: Id;
+	tags: string[];
+	name: string;
+	position: number;
 };
 
 export type BoardList = {
-  id: Id;
-  name: string;
-  items: Id[];
-  position: number;
+	id: Id;
+	name: string;
+	items: Id[];
+	position: number;
 };
 
 export type BoardState = {
@@ -34,71 +34,65 @@ export type BoardState = {
 };
 
 export type BoardListRaw = {
-  id: Id;
-  name: string;
-  items: BoardItem[];
-  position: number;
+	id: Id;
+	name: string;
+	items: BoardItem[];
+	position: number;
 };
 
 export type BoardRaw = {
-  id: Id;
-  name: string;
-  lists: BoardListRaw[];
+	id: Id;
+	name: string;
+	lists: BoardListRaw[];
 };
 
 export interface BoardLoadSuccess {
-  type: BOARD_LOADED_SUCCESS;
-  payload: BoardRaw;
+	type: BOARD_LOADED_SUCCESS;
+	payload: BoardRaw;
 }
 
 export const boardLoad = (): ThunkAction<void, BoardState, void, BoardLoadSuccess> => dispatch => {
-  setTimeout(() => dispatch(boardLoadedSuccess(data)));
+	setTimeout(() => dispatch(boardLoadedSuccess(data)));
 };
 
 export const boardLoadedSuccess = (payload: BoardRaw): BoardLoadSuccess => ({
-  type: BOARD_LOADED_SUCCESS,
-  payload,
+	type: BOARD_LOADED_SUCCESS,
+	payload,
 });
 
 const initialState = {
-  id: null,
-  name: null,
-  items: {},
-  lists: {},
-  listsIds: [],
+	id: null,
+	name: null,
+	items: {},
+	lists: {},
+	listsIds: [],
 };
 
 export default function board(
-  state: BoardState = initialState,
-  { type, payload }: BoardLoadSuccess
+	state: BoardState = initialState,
+	{ type, payload }: BoardLoadSuccess
 ): BoardState {
-  switch (type) {
-    case BOARD_LOADED_SUCCESS: {
-      const [listsIds, rawLists] = normalizeArrayOfObjectsByKey<BoardListRaw, 'id'>(
-        payload.lists,
-        'id'
-      );
-      const items: HashMap<BoardItem> = {};
-      const lists: HashMap<BoardList> = {};
+	switch (type) {
+		case BOARD_LOADED_SUCCESS: {
+		const [listsIds, rawLists] = normalizeArrayOfObjectsByKey<BoardListRaw, 'id'>(payload.lists, 'id');
+		const items: HashMap<BoardItem> = {};
+		const lists: HashMap<BoardList> = {};
 
-      listsIds.forEach(key => {
-        const list = rawLists[key];
-        const [itemsIds, listItems] = normalizeArrayOfObjectsByKey<BoardItem, 'id'>(
-          list.items,
-          'id'
-        );
+		listsIds.forEach(key => {
+			const list = rawLists[key];
+			const [itemsIds, listItems] = normalizeArrayOfObjectsByKey<BoardItem, 'id'>(list.items, 'id');
+			
+			lists[key] = {
+				...list,
+				items: itemsIds,
+			};
 
-        lists[key] = {
-          ...list,
-          items: itemsIds,
-        };
+			Object.assign(items, listItems);
+		});
 
-        Object.assign(items, listItems);
-      });
-
-      return { ...state, lists, items, listsIds };
-    }
-    default:
-      return state;
-  }
+		return { ...state, lists, items, listsIds };
+		}
+		default:
+		return state;
+	}
 }
